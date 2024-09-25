8252a5e94a0affe9983cab8b0178a57241eb1fed
polarnabat <95424973+PAS1865@users.noreply.github.com>
2024-09-25 18:00:53 +0200

    Update la base de script.bat

--- script.bat
+++ script.bat
@@ -1,7 +1,6 @@
 @ echo off
 
-@echo off
-TITLE Bienvenid@ %USERNAME% a @lm_blog menu
+TITLE Bienvenido %USERNAME% a nuestra primera script del menu
 MODE con:cols=80 lines=40
 
 :inicio
@@ -13,21 +12,19 @@ echo -----------------------------------------------------
 echo  1    Opcion 1  
 echo  2    Opcion 2  
 echo  3    Opcion 3  
-echo  4    Opcion 4  
-echo  5    Opcion 5  
-echo  6    Salir
+echo  4    Opcion 4   
+echo  5    Salir
 echo -----------------------------------------------------
 echo.
 
-SET /p var= ^> Seleccione una opcion [1-6]:
+SET /p var= ^> Seleccione una opcion [1-5]:
 
 if "%var%"=="0" goto inicio
 if "%var%"=="1" goto op1
 if "%var%"=="2" goto op2
 if "%var%"=="3" goto op3
 if "%var%"=="4" goto op4
-if "%var%"=="5" goto op5
-if "%var%"=="6" goto salir
+if "%var%"=="5" goto salir
 
 ::Mensaje de error, validación cuando se selecciona una opción fuera de rango
 echo. El numero "%var%" no es una opcion valida, por favor intente de nuevo.
@@ -72,16 +69,6 @@ goto:inicio
     echo.
         ::Aquí van las líneas de comando de tu opción
         color 0B
-    echo.
-    pause
-    goto:inicio
-
-:op5
-    echo.
-    echo. Has elegido la opcion No. 5
-    echo.
-        ::Aquí van las líneas de comando de tu opción
-        color 0C
     echo.
     pause
     goto:inicio
