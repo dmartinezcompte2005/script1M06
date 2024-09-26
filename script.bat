ef1fd9aac931caf2ad4d8ac32d0a632a7571c857
polarnabat <95424973+PAS1865@users.noreply.github.com>
2024-09-26 22:45:17 +0200

    Funcion 3 i 4 funcionan correctamente

--- script.bat
+++ script.bat
@@ -25,80 +25,96 @@ IF "%1" NEQ "" (
 )
 
 :inicio
-cls
-echo -----------------------------------------------------
-echo  %DATE% ^| %TIME%
-echo -----------------------------------------------------
-echo  1    Crear copia de seguridad  
-echo  2    Crear usuario
-echo  3    Modificar targeta de red  
-echo  4    Borrar archivos .log generados
-echo  5    Salir
-echo -----------------------------------------------------
-echo.
+    cls
+    echo ----------------------------------------------------------------------
+    echo  %DATE% ^| %TIME%
+    echo ----------------------------------------------------------------------
+    echo  1    Crear copia de seguridad  
+    echo  2    Crear usuario
+    echo  3    Modificar targeta de red (Ejecutar con administrador)
+    echo  4    Borrar archivos del log generados
+    echo  5    Salir
+    echo ----------------------------------------------------------------------
+    echo.
 
-IF "%var%"=="0" (
-    SET /p var= ^> Seleccione una opcion [1-5]:
-)
+    IF "%var%"=="0" (
+        SET /p var= ^> Seleccione una opcion [1-5]:
+    )
 
-if "%var%"=="1" goto op1
-if "%var%"=="2" goto op2
-if "%var%"=="3" goto op3
-if "%var%"=="4" goto op4
-if "%var%"=="5" goto salir
+    if "%var%"=="1" goto op1
+    if "%var%"=="2" goto op2
+    if "%var%"=="3" goto op3
+    if "%var%"=="4" goto op4
+    if "%var%"=="5" goto salir
 
-::Mensaje de error, validación cuando se selecciona una opción fuera de rango
-echo. El numero "%var%" no es una opcion valida, por favor intente de nuevo.
-echo.
-pause
-set var=0
-goto inicio
+    ::Mensaje de error, validación cuando se selecciona una opción fuera de rango
+    echo. El numero "%var%" no es una opcion valida, por favor intente de nuevo.
+    echo.
+    pause
+    set var=0
+    goto inicio
 
 :op1
-echo. Has elegido la opcion No. 1 >> "%logfile%"
-echo. Creando copia de seguridad... >> "%logfile%"
-:: Aquí iría el código para crear la copia de seguridad
-echo. Copia de seguridad creada con éxito. >> "%logfile%"
-pause
-set var=0
-goto inicio
+        echo. Has elegido la opcion No. 1 >> "%logfile%"
+        echo. Creando copia de seguridad... >> "%logfile%"
+    :: Aquí iría el código para crear la copia de seguridad
+        echo. Copia de seguridad creada con éxito. >> "%logfile%"
+        pause
+        set var=0
+        goto inicio
 
 :op2
-echo. Has elegido la opcion No. 2 >> "%logfile%"
-echo. Creando usuario... >> "%logfile%"
-:: Aquí iría el código para crear el usuario
-set /p username=Ingrese el nombre del usuario:
-:: Aquí iría el código para usar el nombre del usuario
-echo. Usuario %username% creado con éxito. >> "%logfile%"
-pause
-set var=0
-goto inicio
+        echo. Has elegido la opcion No. 2 >> "%logfile%"
+        echo. Creando usuario... >> "%logfile%"
+    :: Aquí iría el código para crear el usuario
+        set /p username=Ingrese el nombre del usuario:
+    :: Aquí iría el código para usar el nombre del usuario
+        echo. Usuario %username% creado con éxito. >> "%logfile%"
+        pause
+        set var=0
+    goto inicio
 
 :op3
-echo. Has elegido la opcion No. 3 >> "%logfile%"
-echo. Modificando tarjeta de red... >> "%logfile%"
-:: Aquí iría el código para modificar la tarjeta de red
-echo. Tarjeta de red modificada con éxito. >> "%logfile%"
-pause
-set var=0
+    echo. Has elegido la opcion No. 3 >> "%logfile%"
+    echo. Modificando tarjeta de red... >> "%logfile%"
+
+    echo. Listando tarjetas de red disponibles... >> "%logfile%"
+    netsh interface show interface
+
+    set /p interface="Introduce el nombre de la interfaz que deseas modificar: "
+    set /p ip="Introduce la nueva IP: "
+    set /p mask="Introduce la máscara de subred: "
+    set /p gateway="Introduce el gateway: "
+
+    echo. Configurando la tarjeta de red con la siguiente información: >> "%logfile%"
+    echo. Interfaz: %interface% >> "%logfile%"
+    echo. IP: %ip% >> "%logfile%"
+    echo. Máscara de subred: %mask% >> "%logfile%"
+    echo. Gateway: %gateway% >> "%logfile%"
+
+    netsh interface ip set address name="%interface%" static %ip% %mask% %gateway%
+
+    echo. Tarjeta de red modificada con éxito. >> "%logfile%"
+    pause
+    set var=0
 goto inicio
 
 :op4
-echo. Has elegido la opcion No. 4
-echo. Mostrando archivos .txt generados... 
-:: Mostrar archivos .txt
-dir "%logdir%\*.txt" /S
-echo. Borrando archivos .txt generados... 
-:: Borrar archivos .txt de forma recursiva
-del /S /Q "%logdir%\*.txt"
-echo. Archivos .txt borrados con éxito. 
-pause
-set var=0
-goto inicio
+        echo. Has elegido la opcion No. 4
+        echo. Mostrando archivos .txt generados... 
+    :: Mostrar archivos .txt
+        dir "%logdir%\*.txt" /S
+        echo. Borrando archivos .txt generados... 
+    :: Borrar archivos .txt de forma recursiva
+        del /S /Q "%logdir%\*.txt"
+        echo. Archivos .txt borrados con éxito. 
+        pause
+        set var=0
+    goto inicio
 
 :salir
-echo Saliendo del script...
-pause
-goto fin
+    echo Saliendo del script...
+    pause
+    goto fin
 
 :fin
