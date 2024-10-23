89285c1b608884355adf11b6c1fe3f5a313941cd
polarnabat <95424973+PAS1865@users.noreply.github.com>
2024-10-23 17:51:32 +0200

    Update gestio_usuaris.sh

--- scriptAvançades/gestio_usuaris.sh
+++ scriptAvançades/gestio_usuaris.sh
@@ -1,1 +1,32 @@
+while true; do
 
+clear
+
+echo "BENVINGUTS A LA PRIMERA OPCIO"
+echo "1. Gestio d'usuaris"
+echo "2. Gestio de copias de seguretat"
+echo "3. "
+echo "4. "
+echo "5. Salir"
+read -p "Indica quina opcio vols triar: " opcio
+
+case "$opcio" in
+
+1)
+	# Introduim el nom del primer fitxer
+	bash gestio_usuaris.sh;;
+2)
+	# Introduim el nom del segon fitxer
+	bash gestio_backups.sh;;
+3)
+	# Introduim el nom del tercer fitxer
+	tercer;;
+4)
+	# Introduim el nom del quart fitxer
+ 	quart;;
+5)
+	exit;;
+
+esac
+	read -p "Prem el enter per continuar"
+done
