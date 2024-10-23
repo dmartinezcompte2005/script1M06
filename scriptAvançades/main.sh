82403c88ff5be645df6fd5a5216e2440e80957f2
polarnabat <95424973+PAS1865@users.noreply.github.com>
2024-10-23 17:39:43 +0200

    Update main.sh

--- scriptAvançades/main.sh
+++ scriptAvançades/main.sh
@@ -1,1 +1,22 @@
+while true; do
 
+clear
+
+echo "BENVINGUTS AL NOSTRE SCRIPT AVANÇAT"
+echo "1. Gestio d'usuaris"
+echo "2. "
+redad -p "Indica quina opcio vols triar: " opcio
+
+case "$opcio" in
+
+1)
+	primer;;
+2)
+	segona;;
+3)
+	exit;;
+
+
+esac
+	read -p "Prem el enter per continuar"
+done
