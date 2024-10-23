648976d5111dd4c023d26e0bd5ea86e4ccd6c631
polarnabat <95424973+PAS1865@users.noreply.github.com>
2024-10-23 17:54:12 +0200

    Create gestio_backups.sh

---
+++ scriptAvançades/gestio_backups.sh
@@ -0,0 +1,33 @@
+while true; do
+
+clear
+
+echo "BENVINGUTS AL SCRIPT DE GESTIO DE COPIES DE SEGURETAT"
+echo "1. Gestio d'usuaris"
+echo "2. Gestio de copias de seguretat"
+echo "3. "
+echo "4. "
+echo "5. Salir"
+read -p "Indica quina opcio vols triar: " opcio
+
+case "$opcio" in
+
+1)
+	# Introduim el nom del primer fitxer
+	bash gestio_usuaris.sh;;
+2)
+	# Introduim el nom del segon fitxer
+	bash gestio_backups.sh;;
+3)
+	# Introduim el nom del tercer fitxer
+	tercer;;
+4)
+	# Introduim el nom del quart fitxer
+ 	quart;;
+5)
+	exit;;
+
+
+esac
+	read -p "Prem el enter per continuar"
+done
