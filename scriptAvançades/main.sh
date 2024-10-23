b6f8bc2af6935457b5c16bc09b2d29fa51e925a8
polarnabat <95424973+PAS1865@users.noreply.github.com>
2024-10-23 17:42:04 +0200

    Update main.sh

--- scriptAvançades/main.sh
+++ scriptAvançades/main.sh
@@ -5,15 +5,26 @@ clear
 echo "BENVINGUTS AL NOSTRE SCRIPT AVANÇAT"
 echo "1. Gestio d'usuaris"
 echo "2. "
+echo "3. "
+echo "4. "
+echo "5. Salir"
 redad -p "Indica quina opcio vols triar: " opcio
 
 case "$opcio" in
 
 1)
+	# Introduim el nom del primer fitxer
 	primer;;
 2)
+	# Introduim el nom del segon fitxer
 	segona;;
 3)
+	# Introduim el nom del tercer fitxer
+	tercer;;
+4)
+	# Introduim el nom del quart fitxer
+ 	quart;;
+5)
 	exit;;
 
 
